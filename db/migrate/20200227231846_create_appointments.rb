a9d7defed8ae0f943af0acf2a196b02e5a3097d3
Riley Slayden <slaydenriley@gmail.com>
2020-02-27 23:20:01 +0000

    Automatically backed up by Learn

--- db/migrate/20200227231846_create_appointments.rb
+++ db/migrate/20200227231846_create_appointments.rb
@@ -1,8 +1,7 @@
 class CreateAppointments < ActiveRecord::Migration[5.0]
   def change
     create_table :appointments do |t|
-      t.integer :date
-      t.integer :time
+      t.integer :appointment_datetime
 
       t.timestamps
     end
