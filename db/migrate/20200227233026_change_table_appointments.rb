9067f2c2e345665c5cee80a058f27ca815da26a7
Riley Slayden <slaydenriley@gmail.com>
2020-02-27 23:31:26 +0000

    Automatically backed up by Learn

--- db/migrate/20200227233026_change_table_appointments.rb
+++ db/migrate/20200227233026_change_table_appointments.rb
@@ -2,5 +2,6 @@ class ChangeTableAppointments < ActiveRecord::Migration[5.0]
   def change
     change_table :appointments do |t|
       t.change :appointment_datetime, :datetime
+    end
   end
 end
