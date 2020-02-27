3884d1b0003c3f75c5a48d2837d7343139354fed
Riley Slayden <slaydenriley@gmail.com>
2020-02-27 23:31:35 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200227231918) do
+ActiveRecord::Schema.define(version: 20200227233026) do
 
   create_table "appointments", force: :cascade do |t|
-    t.string   "appointment_datetime"
+    t.datetime "appointment_datetime"
     t.integer  "doctor_id"
     t.integer  "patient_id"
     t.datetime "created_at",           null: false
